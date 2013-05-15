21c65f31ab812bc522e899473e5698512e27ad6b
ChrisWren <cthewren@gmail.com>
2013-05-14 22:13:10 -0700

    added file creation test

---
+++ test/fileCreationTest.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+require('should');
+var spawn = require('child_process').spawn;
+
+before(function (done) {
+  var buildProcess = spawn('grunt', ['build']);
+  buildProcess.stdout.on('close', function () {
+    done();
+  });
+});
+
+describe('When the grunt blog task is run', function () {
+
+  it('creates posts in the location specified by the url', function() {
+    fs.existsSync('dev/blog/posts/Post1.html').should.be.ok;
+    fs.existsSync('dev/blog/posts/Post2.html').should.be.ok;
+  });
+
+  it('creates posts using the markdown content', function () {
+    fs.readFileSync('dev/blog/posts/Post1.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/posts/Post1.html', 'utf8'));
+    fs.readFileSync('dev/blog/posts/Post2.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/posts/Post2.html', 'utf8'));
+  });
+
+  it('creates pages in the location specified by the url', function() {
+    fs.existsSync('dev/blog/index.html').should.be.ok;
+    fs.existsSync('dev/index.html').should.be.ok;
+  });
+
+  it('creates pages using the page content', function () {
+    fs.readFileSync('dev/index.html', 'utf8').should.equal(fs.readFileSync('test/output/index.html', 'utf8'));
+    fs.readFileSync('dev/blog/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/index.html', 'utf8'));
+  });
+
+});
