617149591b3f84e84673054cdb35f1fb9b28d9a2
ChrisWren <cthewren@gmail.com>
2013-05-27 23:02:08 -0700

    ordered posts by descending timestamp

--- tasks/index.js
+++ tasks/index.js
@@ -1,7 +1,8 @@
+'use strict';
 var fs = require('fs');
 var path = require('path');
-var marked = require('marked');
 var jsYAML = require('js-yaml');
+var marked = require('marked');
 var pygmentize = require('pygmentize-bundled');
 require('colors');
 
@@ -11,47 +12,23 @@ var templateEngines = {
 };
 
 module.exports = function (grunt) {
-  'use strict';
+  var done;
+  var options;
+  var postCollection = [];
+  var templateEngine;
 
   grunt.registerMultiTask('pages', 'Creates pages from markdown and templates.', function () {
-    var done = this.async();
-    var options = this.options();
-    var postsMetadata = [];
-    var templateEngine;
+    done = this.async();
+    options = this.options();
 
     if (this.data.src) {
       var numPosts = grunt.file.expand({ filter: 'isFile' }, this.data.src + '/**').length;
-      var generatedPosts = 0;
+      var parsedPosts = 0;
+
       grunt.file.recurse(this.data.src, function (abspath) {
-        var fileString = fs.readFileSync(abspath, 'utf8');
-        var metadata;
-        var content;
-        // Parse the metadata and content from the post
-        try {
-          // Parse JSON metadata
-          if (fileString.indexOf('{') === 0) {
-            metadata = eval('(' + fileString.substr(0, fileString.indexOf('}') + 1) + ')');
-            metadata.date = new Date(metadata.date);
-            content = fileString.slice(fileString.indexOf('}') + 1);
-          // Parse YAML metadata
-          } else if (fileString.indexOf('----') === 0) {
-            metadata = jsYAML.load(fileString.split('----')[1]);
-            // Extract the content by removing the metadata section
-            var sections = fileString.split('----');
-            sections.shift();
-            sections.shift();
-            content = sections.join();
-          } else {
-            grunt.log.error('Error:'.red + ' the metadata for the following ' + 'post'.blue + ' is formatted incorrectly: ' + abspath.red);
-            return;
-          }
-        } catch (e) {
-          grunt.log.error(e);
-          grunt.log.error('Error:'.red + ' the metadata for the following ' + 'post'.blue + ' is formatted incorrectly: ' + abspath.red);
-          return;
-        }
-        var that = this;
-        marked(content, {
+        var post = parsePostData(abspath);
+        // Parse post using marked and pygmentize for highlighting
+        marked(post.markdown, {
           highlight: function (code, lang, callback) {
             pygmentize({ lang: lang, format: 'html' }, code, function (err, result) {
               callback(err, result.toString());
@@ -59,52 +36,124 @@ module.exports = function (grunt) {
           },
           gfm: true
         }, function (err, content) {
-          metadata.content = content;
-          // Create the url using the config and metadata
-          var dynamicUrlSegments = that.data.url.split('/:');
-          // Ignore the static section of the url
-          if (that.data.url[0] !== ':') dynamicUrlSegments.shift();
-          var dest = that.data.dest + '/' + that.data.url + '.html';
-          // Replace dynamic url segments
-          dynamicUrlSegments.forEach(function (segment) {
-            if (segment in metadata) {
-              dest = dest.replace(':' + segment, metadata[segment].replace(/[^a-zA-Z0-9]/g, '-'));
-            } else {
-              grunt.log.error('Required ' + segment.red + ' attribute not found in ' + 'post'.blue + 'metadata at ' + abspath + '.');
-              return;
-            }
-          });
+          if (err) throw err;
+          delete post.markdown;
+          post.content = content;
+          post.dest = getPostDest(this, post, abspath);
+          post.url = post.dest.slice((this.data.dest + '/').length);
+          postCollection.push(post);
 
-          // Remove the dest folder from the post's relative url
-          metadata.url = dest.slice((that.data.dest + '/').length);
-
-          if (that.data.layout) {
-            // Determine the template engine based on the file's extention name
-            templateEngine = templateEngines[path.extname(that.data.layout).slice(1)];
-            var layoutString = fs.readFileSync(that.data.layout, 'utf8');
-            var fn = templateEngine.compile(layoutString, { pretty: true, filename: that.data.layout });
-            grunt.file.write(dest, fn(metadata));
-            grunt.log.ok('Created ' + 'post'.blue + ' at: ' + dest);
-          } else {
-            grunt.file.write(dest, metadata.content);
-          }
-          postsMetadata.push(metadata);
-
-          if (++generatedPosts === numPosts) {
-            // If the user wants to inject blog data into a page, render the page templates with the post metadata
+          // Once all the source posts are parsed, we can generate the html posts
+          if (++parsedPosts === numPosts) {
+            generatePosts(this, postCollection);
+            // If the user wants to inject post data into a page, render the page templates with the posts' data
             if (options.pageSrc) {
-              grunt.file.recurse(options.pageSrc, function (abspath, rootdir) {
-                var layoutString = fs.readFileSync(abspath, 'utf8');
-                var fn = templateEngine.compile(layoutString, { pretty: true, filename: abspath });
-                var dest = that.data.dest + '/' + abspath.slice(rootdir.length + 1).replace(path.extname(abspath), '.html');
-                grunt.log.ok('Created ' + 'page'.magenta + ' at: ' + dest);
-                grunt.file.write(dest, fn({ posts: postsMetadata }));
-              });
+              generatePages(this, postCollection);
             }
             done();
           }
-        });
+        }.bind(this));
       }.bind(this));
     }
   });
+
+  /**
+   * Parses the metadata and content from a post
+   * @param  {String} abspath
+   * @return {Object}
+   */
+  function parsePostData (abspath) {
+    var fileString = fs.readFileSync(abspath, 'utf8');
+    var postData = {};
+    try {
+      // Parse JSON metadata
+      if (fileString.indexOf('{') === 0) {
+        postData = eval('(' + fileString.substr(0, fileString.indexOf('}') + 1) + ')');
+        postData.date = new Date(postData.date);
+        postData.markdown = fileString.slice(fileString.indexOf('}') + 1);
+      // Parse YAML metadata
+      } else if (fileString.indexOf('----') === 0) {
+        postData = jsYAML.load(fileString.split('----')[1]);
+        // Extract the content by removing the metadata section
+        var sections = fileString.split('----');
+        sections.shift();
+        sections.shift();
+        postData.markdown = sections.join('----');
+      } else {
+        grunt.log.error('Error:'.red + ' the metadata for the following ' + 'post'.blue + ' is formatted incorrectly: ' + abspath.red);
+        done();
+      }
+      return postData;
+    } catch (e) {
+      grunt.log.error(e);
+      grunt.log.error('Error:'.red + ' the metadata for the following ' + 'post'.blue + ' is formatted incorrectly: ' + abspath.red);
+      done();
+    }
+  }
+
+  /**
+   * Returns the post destination based on the url property and postData
+   * @param  {Object} postData
+   * @param  {String} abspath
+   * @return {String}
+   */
+  function getPostDest (that, postData, abspath) {
+    var dest;
+    var dynamicUrlSegments = that.data.url.split('/:');
+    // Ignore the static section of the url
+    if (that.data.url[0] !== ':') dynamicUrlSegments.shift();
+    dest = that.data.dest + '/' + that.data.url + '.html';
+    // Replace dynamic url segments
+    dynamicUrlSegments.forEach(function (segment) {
+      if (segment in postData) {
+        dest = dest.replace(':' + segment, postData[segment].replace(/[^a-zA-Z0-9]/g, '-'));
+      } else {
+        grunt.log.error('Required ' + segment.red + ' attribute not found in ' + 'post'.blue + 'metadata at ' + abspath + '.');
+        return;
+      }
+    });
+    return dest;
+  }
+
+  /**
+   * Generates posts based on the provided data
+   * @param  {Object} postsData
+   * @return {null}
+   */
+  function generatePosts (that, postsData) {
+    if (that.data.layout) {
+      // Sort posts by descending date
+      postsData.sort(function (a, b) {
+        return b.date - a.date;
+      });
+
+      postsData.forEach(function (metadata) {
+        // Determine the template engine based on the file's extention name
+        templateEngine = templateEngines[path.extname(that.data.layout).slice(1)];
+        var layoutString = fs.readFileSync(that.data.layout, 'utf8');
+        var fn = templateEngine.compile(layoutString, { pretty: true, filename: that.data.layout });
+        grunt.file.write(metadata.dest, fn(metadata));
+        grunt.log.ok('Created ' + 'post'.blue + ' at: ' + metadata.dest);
+      });
+    } else {
+      grunt.log.error('Please specify a post layout.');
+      done();
+    }
+  }
+
+  /**
+   * Genereates pages using the posts' data
+   * @param  {Object} postsData
+   * @return {null}
+   */
+  function generatePages (that, postsData) {
+    grunt.file.recurse(options.pageSrc, function (abspath, rootdir) {
+      var layoutString = fs.readFileSync(abspath, 'utf8');
+      var fn = templateEngine.compile(layoutString, { pretty: true, filename: abspath });
+      var dest = that.data.dest + '/' +
+                 abspath.slice(rootdir.length + 1).replace(path.extname(abspath), '.html');
+      grunt.log.ok('Created ' + 'page'.magenta + ' at: ' + dest);
+      grunt.file.write(dest, fn({ posts: postsData }));
+    });
+  }
 };
