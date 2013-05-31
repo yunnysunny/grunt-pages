499068bcdc81939d1a4d25f808e550dd453a071f
ChrisWren <cthewren@gmail.com>
2013-05-30 22:54:09 -0700

    updated test for pagination option

--- test/fileCreationTest.js
+++ test/fileCreationTest.js
@@ -2,14 +2,14 @@ var fs = require('fs');
 require('should');
 var spawn = require('child_process').spawn;
 
-before(function (done) {
-  var buildProcess = spawn('grunt', ['build']);
-  buildProcess.stdout.on('close', function () {
-    done();
-  });
-});
-
 describe('When the grunt blog task is run', function () {
+
+  before(function (done) {
+    var buildProcess = spawn('grunt', ['pages:posts']);
+    buildProcess.stdout.on('close', function () {
+      done();
+    });
+  });
 
   it('creates posts in the location specified by the url', function() {
     fs.existsSync('dev/blog/posts/Post-1.html').should.be.ok;
@@ -29,6 +29,30 @@ describe('When the grunt blog task is run', function () {
   it('creates pages using the page content', function () {
     fs.readFileSync('dev/index.html', 'utf8').should.equal(fs.readFileSync('test/output/index.html', 'utf8'));
     fs.readFileSync('dev/blog/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/index.html', 'utf8'));
+  });
+
+});
+
+describe('When the grunt blog task is run with pagination', function () {
+
+  before(function (done) {
+    var buildProcess = spawn('grunt', ['pages:paginated']);
+    buildProcess.stdout.on('close', function () {
+      done();
+    });
+  });
+
+  it('creates pages in the expected location', function() {
+    fs.existsSync('dev/blog/index.html').should.be.ok;
+    fs.existsSync('dev/blog/page/1/index.html').should.be.ok;
+  });
+
+  it('creates the root list page', function () {
+    fs.readFileSync('dev/blog/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/paginatedIndex.html', 'utf8'));
+  });
+
+  it('creates the paginated list page', function () {
+    fs.readFileSync('dev/blog/page/1/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/page/1/index.html', 'utf8'));
   });
 
 });
