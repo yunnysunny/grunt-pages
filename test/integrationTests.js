dd2013998851cc46c5133e333f48f07744c47b6b
ChrisWren <cthewren@gmail.com>
2013-08-07 21:49:58 -0700

    Moved integration test fixtures into integration folder

--- test/integrationTests.js
+++ test/integrationTests.js
@@ -21,8 +21,8 @@ describe('grunt-pages', function () {
   });
 
   it('should create posts by correctly parsing the markdown content', function () {
-    fs.readFileSync('dev/blog/posts/Post_1/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/posts/Post1/index.html', 'utf8'));
-    fs.readFileSync('dev/blog/posts/Post_2/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/posts/Post2/index.html', 'utf8'));
+    fs.readFileSync('dev/blog/posts/Post_1/index.html', 'utf8').should.equal(fs.readFileSync('test/fixtures/integration/output/blog/posts/Post1/index.html', 'utf8'));
+    fs.readFileSync('dev/blog/posts/Post_2/index.html', 'utf8').should.equal(fs.readFileSync('test/fixtures/integration/output/blog/posts/Post2/index.html', 'utf8'));
   });
 
   it('should create pages in the location specified by the url', function() {
@@ -31,8 +31,8 @@ describe('grunt-pages', function () {
   });
 
   it('should create pages using the page content', function () {
-    fs.readFileSync('dev/index.html', 'utf8').should.equal(fs.readFileSync('test/output/index.html', 'utf8'));
-    fs.readFileSync('dev/blog/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/index.html', 'utf8'));
+    fs.readFileSync('dev/index.html', 'utf8').should.equal(fs.readFileSync('test/fixtures/integration/output/index.html', 'utf8'));
+    fs.readFileSync('dev/blog/index.html', 'utf8').should.equal(fs.readFileSync('test/fixtures/integration/output/blog/index.html', 'utf8'));
   });
 
   it('should ignore _ prefixed draft posts', function () {
@@ -41,7 +41,7 @@ describe('grunt-pages', function () {
 
   it('should cache posts after they have been parsed', function () {
     var post = JSON.parse(fs.readFileSync('.posts-post-cache.json')).posts[0];
-    post.sourcePath.should.equal('test/fixtures/posts/post2.md');
+    post.sourcePath.should.equal('test/fixtures/integration/input/posts/post2.md');
   });
 
   describe('when run with the RSS object set with default options', function () {
@@ -54,7 +54,7 @@ describe('grunt-pages', function () {
     });
 
     it('should create feed.xml from the posts and default options', function () {
-      stripBuildDate(fs.readFileSync('dev/feed.xml', 'utf8')).should.equal(stripBuildDate(fs.readFileSync('test/output/feed.xml', 'utf8')));
+      stripBuildDate(fs.readFileSync('dev/feed.xml', 'utf8')).should.equal(stripBuildDate(fs.readFileSync('test/fixtures/integration/output/feed.xml', 'utf8')));
     });
   });
 
@@ -68,7 +68,7 @@ describe('grunt-pages', function () {
     });
 
     it('should create rss.xml from the posts and provided options', function () {
-      stripBuildDate(fs.readFileSync('dev/rss/rss.xml', 'utf8')).should.equal(stripBuildDate(fs.readFileSync('test/output/rss/rss.xml', 'utf8')));
+      stripBuildDate(fs.readFileSync('dev/rss/rss.xml', 'utf8')).should.equal(stripBuildDate(fs.readFileSync('test/fixtures/integration/output/rss/rss.xml', 'utf8')));
     });
   });
 
@@ -90,15 +90,15 @@ describe('grunt-pages', function () {
     });
 
     it('should create the a custom list page with the expected content', function () {
-      fs.readFileSync('dev/page/javascript/index.html', 'utf8').should.equal(fs.readFileSync('test/output/page/javascript/index.html', 'utf8'));
+      fs.readFileSync('dev/page/javascript/index.html', 'utf8').should.equal(fs.readFileSync('test/fixtures/integration/output/page/javascript/index.html', 'utf8'));
     });
 
     it('should create the root list page with the expected content', function () {
-      fs.readFileSync('dev/index.html', 'utf8').should.equal(fs.readFileSync('test/output/blog/paginatedIndex.html', 'utf8'));
+      fs.readFileSync('dev/index.html', 'utf8').should.equal(fs.readFileSync('test/fixtures/integration/output/blog/paginatedIndex.html', 'utf8'));
     });
 
     it('should create the paginated list page with the expected content', function () {
-      fs.readFileSync('dev/list/1/index.html', 'utf8').should.equal(fs.readFileSync('test/output/list/1/index.html', 'utf8'));
+      fs.readFileSync('dev/list/1/index.html', 'utf8').should.equal(fs.readFileSync('test/fixtures/integration/output/list/1/index.html', 'utf8'));
     });
 
   });
