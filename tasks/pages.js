0b851b1223aa2191627f32ff6a77cf45130936c1
ChrisWren <cthewren@gmail.com>
2013-10-23 08:37:39 -0700

    List node-fs in correct location in the list of requires

--- tasks/pages.js
+++ tasks/pages.js
@@ -12,9 +12,9 @@ var path = require('path');
 var url  = require('url');
 
 require('colors');
-var fs         = require('node-fs');
 var _          = require('lodash');
 var marked     = require('marked');
+var fs         = require('node-fs');
 var pygmentize = require('pygmentize-bundled');
 var RSS        = require('rss');
 
